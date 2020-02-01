7dc5d511c9f4a22e9e5b00867465fc7cffcb8295
Oksanny <oksana.voitko7@gmail.com>
2020-02-01 21:54:27 +0200

    load project

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    public Vector3 plaftormPosition;
+    
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    { 
+        plaftormPosition.x += Input.GetAxis("Horizontal"); // (10,0,0);
+        gameObject.transform.position = plaftormPosition;
+        Debug.Log(Input.GetAxis("Horizontal"));
+    }
+}
