665b26137d072684e498e4b6895b9172031b1235
RobocodeUnity <60553711+RobocodeUnity@users.noreply.github.com>
2020-02-09 12:36:43 +0300

    added boundary

---
+++ Assets/Scenes/BallMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMover : MonoBehaviour
+{
+
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+
+    // Use this for initialization
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
